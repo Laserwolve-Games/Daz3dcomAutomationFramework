d3dc157395e4e1366de5357caf4ae73ba4166319
Andrew Rogers <AndrewRogers@LaserwolveGames.com>
2023-03-02 10:40:58 -0800

    Reorganizing folders and files

--- src/daz3dcomAutomation/Data.java
+++
@@ -1,9 +0,0 @@
-package daz3dcomAutomation;
-
-public class Data
-{
-	public static final String mainURL = "https://www.daz3d.com/";
-	public static final String shopURL = mainURL + "shop/";
-	public static final String driverKey = "webdriver.edge.driver";
-	public static final String driverPath = "C:/driver/msedgedriver.exe";
-}
--- src/daz3dcomAutomation/SensitiveData.java
+++
@@ -1,7 +0,0 @@
-package daz3dcomAutomation;
-
-public class SensitiveData
-{
-	public static final String emailAddress = "laserwolve@gmail.com";
-	public static final String password = "Vt)Y5X+?anK+v!y";
-}
